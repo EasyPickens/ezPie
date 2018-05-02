2a7a4a6622a55f05b9eb35aee7f288d2cd10b3e3
r0urpm <richard_monson@fanniemae.com>
2018-05-02 08:47:48 -0400

    Added awarded registered trademark note to data file classes.

--- JAVA/ezPIE/ezpie/src/main/java/com/fanniemae/ezpie/datafiles/lowlevel/FieldClob.java
+++ JAVA/ezPIE/ezpie/src/main/java/com/fanniemae/ezpie/datafiles/lowlevel/FieldClob.java
@@ -1,8 +1,26 @@
+/**
+ *  
+ * Copyright (c) 2018 Fannie Mae, All rights reserved.
+ * This program and the accompany materials are made available under
+ * the terms of the Fannie Mae Open Source Licensing Project available 
+ * at https://github.com/FannieMaeOpenSource/ezPie/wiki/License
+ * 
+ * ezPIE® is a registered trademark of Fannie Mae
+ * 
+ */
+
 package com.fanniemae.ezpie.datafiles.lowlevel;
 
 import java.io.IOException;
 import java.sql.Clob;
 import java.sql.SQLException;
+
+/**
+ * 
+ * @author Rick Monson (richard_monson@fanniemae.com, https://www.linkedin.com/in/rick-monson/)
+ * @since 2018-02-12
+ * 
+ */
 
 public class FieldClob extends FieldReadWrite {
 
