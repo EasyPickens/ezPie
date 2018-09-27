b44b1fe8619fc0941a30d3ecfeb58ea99bfb53cc
EasyPickens <easypickens@live.com>
2018-09-27 15:43:54 -0400

    Added newly created SpringBoot REST wrapper for ezPIE.  The project is currently called warmpie.  Still adding methods and needs a post method to accept parameters and other options.

---
+++ JAVA/ezPIE/warmpie/src/main/java/com/rest/wrapper/ezpie/warmpie/WarmpieApplication.java
@@ -0,0 +1,12 @@
+package com.rest.wrapper.ezpie.warmpie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmpieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmpieApplication.class, args);
+	}
+}
---
+++ JAVA/ezPIE/warmpie/src/main/java/com/rest/wrapper/ezpie/warmpie/controllers/PieController.java
@@ -0,0 +1,39 @@
+package com.rest.wrapper.ezpie.warmpie.controllers;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+//import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fanniemae.ezpie.JobManager;
+
+@RestController
+public class PieController {
+
+	@Value("${ezpie.settings}")
+	private String _pathToSettings;
+
+	@GetMapping("/api/v1/status")
+	public String getStatus() {
+		return "WarmPie REST API is up and running.";
+	}
+
+	@GetMapping("/api/v1/runjob/{jobname}")
+	public ResponseEntity<Object> runJob(@PathVariable String jobname) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(String.format("%s: INFO GET request to run %s job definition.", sdf.format(Calendar.getInstance().getTime()), jobname));
+		System.out.println(String.format("%s: INFO Path to settings file: %s", sdf.format(Calendar.getInstance().getTime()), _pathToSettings));
+
+		JobManager jm = new JobManager(_pathToSettings, jobname, null);
+		String data = jm.getDataJson();
+		return new ResponseEntity<Object>(data,HttpStatus.OK);
+	}
+
+}
